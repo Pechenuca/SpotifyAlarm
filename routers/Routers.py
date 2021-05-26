ce533e265ed8588fc0b0d803151e91483ee6789d
Mark Tamarov <googlcraftlol@gmail.com>
2021-05-26 10:22:37 +0300

    feat(routers): added post request

--- routers/Routers.py
+++ routers/Routers.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 from models import DataBaseWrapper, Requset
 from utils import organizer
+from flask import request, jsonify
 
 class BasicRoots(Resource):
     """
@@ -14,12 +15,27 @@ class BasicRoots(Resource):
         db_wrapper = DataBaseWrapper(Requset)
         return organizer(db_wrapper.select_all())
     
-    def post(self):
+    def post(self) -> str:
         """
         Post запрос.\n
-        Добовляет запрос(будильник).
+        Добовляет запрос(будильник).\n
+        Пример запроса: {"alarm_time": "2021-05-26 10:11:00.940031", "playlist": "https://google.com", "is_worked": 0}.\n
+        В случае ошибки в запросе возращяет Exception.
         """
-        pass
-        # TODO доделать пост запрос
+        json_data = request.get_json(force=True)
+        
+        try:
+            alarm = json_data["alarm_time"]
+            playlist = json_data["playlist"]
+            is_worked = json_data["is_worked"]
+        except Exception as e:
+            return f"Check your request! Missing {e}"
+
+        db_wrapper = DataBaseWrapper(Requset)
+        db_wrapper.insert(alarm_time=alarm, playlist=playlist, is_worked=is_worked)
+        return f"Data added to database!"
+        
+        
+
 
     
